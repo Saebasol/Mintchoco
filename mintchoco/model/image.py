04e0c3ea2db18dea62f3a20dbd93c494092ba367
YongChan Cho <h3236516@gmail.com>
2022-01-30 02:48:12 +0900

    refactor(image): refactoring image dataclass

--- mintchoco/model/image.py
+++ mintchoco/model/image.py
@@ -1,16 +1,8 @@
 from dataclasses import dataclass
 
-from mintchoco.types import HeliotropeImageJSON
+from mintchoco.base import HeliotropeFileJSON
 
 
 @dataclass
 class Image:
-    name: str
-    url: str
-
-    def to_dict(self) -> HeliotropeImageJSON:
-        return HeliotropeImageJSON(name=self.name, url=self.url)
-
-    @classmethod
-    def from_dict(cls, d: HeliotropeImageJSON) -> "Image":
-        return cls(name=d["name"], url=d["url"])
+    files: list[HeliotropeFileJSON]
